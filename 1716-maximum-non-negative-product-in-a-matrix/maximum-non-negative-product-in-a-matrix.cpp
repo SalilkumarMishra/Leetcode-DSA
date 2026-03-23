1a7dabdf636d47fefc2c16e76505cab418caa86f
SalilkumarMishra <147610541+SalilkumarMishra@users.noreply.github.com>
2026-03-24 01:58:06 +0530

    Time: 3 ms (34.08%) | Memory: 14.1 MB (54.71%) - LeetSync

--- 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
+++ 1716-maximum-non-negative-product-in-a-matrix/maximum-non-negative-product-in-a-matrix.cpp
@@ -1,33 +1,51 @@
 class Solution {
- public:
-  int maxProductPath(vector<vector<int>>& grid) {
-    constexpr int kMod = 1'000'000'007;
-    const int m = grid.size();
-    const int n = grid[0].size();
-    // dpMin[i][j] := the minimum product from (0, 0) to (i, j)
-    // dpMax[i][j] := the maximum product from (0, 0) to (i, j)
-    vector<vector<long>> dpMin(m, vector<long>(n));
-    vector<vector<long>> dpMax(m, vector<long>(n));
+public:
+    typedef long long ll;
+    int MOD = 1e9 + 7;
+    vector<vector<pair<ll, ll>>> t;
+    int m, n;
 
-    dpMin[0][0] = dpMax[0][0] = grid[0][0];
+    int maxProductPath(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
 
-    for (int i = 1; i < m; ++i)
-      dpMin[i][0] = dpMax[i][0] = dpMin[i - 1][0] * grid[i][0];
+        // bottom up 
+        vector<vector<pair<ll, ll>>> t(m, vector<pair<ll, ll>>(n));
+        // t[i][j] = {maxVal, minVal} to reach from (0, 0) to (i, j)
 
-    for (int j = 1; j < n; ++j)
-      dpMin[0][j] = dpMax[0][j] = dpMin[0][j - 1] * grid[0][j];
+        t[0][0] = {grid[0][0], grid[0][0]};
 
-    for (int i = 1; i < m; ++i)
-      for (int j = 1; j < n; ++j)
-        if (grid[i][j] < 0) {
-          dpMin[i][j] = max(dpMax[i - 1][j], dpMax[i][j - 1]) * grid[i][j];
-          dpMax[i][j] = min(dpMin[i - 1][j], dpMin[i][j - 1]) * grid[i][j];
-        } else {
-          dpMin[i][j] = min(dpMin[i - 1][j], dpMin[i][j - 1]) * grid[i][j];
-          dpMax[i][j] = max(dpMax[i - 1][j], dpMax[i][j - 1]) * grid[i][j];
+        // filling the first row
+        for (int j = 1; j < n; j++) {
+            t[0][j].first = t[0][j - 1].first * grid[0][j];   // maxVal
+            t[0][j].second = t[0][j - 1].second * grid[0][j]; // minVal
         }
 
-    const long mx = max(dpMin.back().back(), dpMax.back().back());
-    return mx < 0 ? -1 : mx % kMod;
-  }
+        // filling the first column
+        for (int i = 1; i < m; i++) {
+            t[i][0].first = t[i - 1][0].first * grid[i][0];   // max Val
+            t[i][0].second = t[i - 1][0].second * grid[i][0]; // min val
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                ll upMax = t[i - 1][j].first;
+                ll upMin = t[i - 1][j].second;
+
+                ll leftMax = t[i][j - 1].first;
+                ll leftMin = t[i][j - 1].second;
+
+                t[i][j].first =
+                    max({upMax * grid[i][j], upMin * grid[i][j],
+                         leftMax * grid[i][j], leftMin * grid[i][j]});
+                t[i][j].second =
+                    min({upMax * grid[i][j], upMin * grid[i][j],
+                         leftMax * grid[i][j], leftMin * grid[i][j]});
+            }
+        }
+
+        auto [maxProd, minProd] = t[m - 1][n - 1];
+
+        return maxProd < 0 ? -1 : maxProd % MOD;
+    }
 };
