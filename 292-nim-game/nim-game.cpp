254dda7f2c12c2c0b78ca76976c100605f1502d4
SalilkumarMishra <147610541+SalilkumarMishra@users.noreply.github.com>
2026-06-22 14:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (51.21%) - LeetSync

---
+++ 292-nim-game/nim-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n<3)
+        {
+            return true;
+        }
+        
+        if(n%4!=0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
