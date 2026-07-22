88864ea0ebc52f370e0afef3a7e637255febe9f0
SalilkumarMishra <147610541+SalilkumarMishra@users.noreply.github.com>
2026-07-22 22:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (78.91%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-int dp[101];
-    int solve(int i, vector<int> &nums)
+// int dp[101];
+//     int solve(int i, vector<int> &nums)
+//     {
+//         if(i>=nums.size())
+//         {
+//             return 0;
+//         }
+//         if(dp[i]!=-1)
+//         {
+//             return dp[i];
+//         }
+//         int rob = nums[i] + solve(i+2,nums);
+//         int skip = solve(i+1,nums);
+//         return dp[i] = max(rob,skip);
+//     }
+//     int rob(vector<int>& nums) {
+//         memset(dp,-1,sizeof(dp));
+//         return solve(0,nums);
+//     }
+    int rob(vector<int>& nums)
     {
-        if(i>=nums.size())
+        int next1 = 0;
+        int next2 = 0;
+        for(int i=nums.size()-1;i >= 0;i--)
         {
-            return 0;
-        }
-        if(dp[i]!=-1)
-        {
-            return dp[i];
-        }
-        int rob = nums[i] + solve(i+2,nums);
-        int skip = solve(i+1,nums);
-        return dp[i] = max(rob,skip);
-    }
-    int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,nums);
-    }
+            int current = max(nums[i]+next2, next1);
+            next2 = next1;
+            next1 = current;
+        } 
+        return next1;  
+}
 };
